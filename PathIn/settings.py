a39bdbc0e7e580f83dda0c2486ba30e51a44a59b
MubarakHus <125471014+MubarakHus@users.noreply.github.com>
2024-04-19 13:34:42 +0300

    Update settings.py

--- PathIn/settings.py
+++ PathIn/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4lid7lzy=!4=)z_1g@gt&ht=inn^n)@n!k724@j-c4pqcr$!x!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
