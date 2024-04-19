9704a887fa6861628e024a11d93f7449a39fbbcf
Your Name <mmm19mmm@outlook.com>
2024-04-19 13:08:05 +0300

    1.1

--- PathIn/settings.py
+++ PathIn/settings.py
@@ -27,8 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://web-production-9ba3.up.railway.app/']
 # Application definition
 
 INSTALLED_APPS = [
