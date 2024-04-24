dcb9c87e6bf72f5dad5511e869a8946a282b193f
Your Name <mmm19mmm@outlook.com>
2024-04-24 06:01:14 +0300

    1.1

--- navApp/urls.py
+++ navApp/urls.py
@@ -6,7 +6,8 @@ from django.conf import settings
 urlpatterns = [
     path('',  views.index, name='index'),
     path('term/',  views.autocomplete, name='autocomplete'),
-    path('path/', views.search_dij, name='search_dij'),
+    path('path/<str:src>/<str:gol>', views.search_dij, name='search_dij'),
+    path('path/<str:source>/', views.dynamic_url, name='dynamic_url'),
     path('generate-home/', views.gen_view, name='gen'),
     path('generate-qr/<str:location>/', views.generate_qr, name='generate-qr'),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
